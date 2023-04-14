13972121bba013708615e4dcf99e4ad0ae859b1b
Hridayesh Akhand <hridayesh.akhand@gmail.com>
2023-04-15 08:07:29 +0900

    resolved conflict

--- src/componets/navbar.js
+++ src/componets/navbar.js
@@ -3,17 +3,14 @@ import { Flex, Spacer, Button, Image, Box } from '@chakra-ui/react';
 import Link from 'next/link';
 import { Profile } from '@/componets/profile';
 
-
 const Navbar = () => {
-  return (
-    <Flex as="nav" align="center" justify="space-between" padding="1rem">
-      <Link href="/">
-        <Box as="a" cursor="pointer">
-          <Image src="/src/assets/logo.png" alt="Logo" boxSize="40px" />
-        </Box>
-	  </Link>
-	  <Spacer />
-	  {/*<Link href="/">
+	return (
+		<Flex as='nav' align='center' justify='space-between' padding='1rem'>
+			<Link href='/'>
+				<Image src='/assets/logo.png' alt='Logo' boxSize='40px' />
+			</Link>
+			<Spacer />
+			{/*<Link href="/">
         <Button variant="ghost">Home</Button>
       </Link>
       <Link href="/about">
@@ -22,10 +19,9 @@ const Navbar = () => {
       <Link href="/contact">
         <Button variant="ghost">Contact</Button>
       </Link>*/}
-	  <Profile />
-    </Flex>
-  )
-}
+			<Profile />
+		</Flex>
+	);
+};
 
 export default Navbar;
-
